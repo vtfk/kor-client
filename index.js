a0d4f278168051059337df857e153f9fc057fdd9
Jonas Enge <jonas.enge@gmail.com>
2018-01-19 15:50:49 +0100

    Genesis commit

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.createJwt = require('./lib/create-jwt')
+exports.getToken = require('./lib/get-token')
+exports.getData = require('./lib/get-data')
---
+++ tst.js
@@ -0,0 +1,31 @@
+(async () => {
+  const { readFileSync } = require('fs')
+  const { createJwt, getToken, getData } = require('./index')
+
+  try {
+    const jwtOptions = {
+      cert: readFileSync('./data/cert.crt', 'utf-8'),
+      privateKey: readFileSync('./data/private.key', 'utf-8'),
+      issuer: 'TFK',
+      scope: 'global/kontaktinformasjon.read'
+    }
+
+    const tokenOptions = {
+      url: 'https://oidc-yt2.difi.eon.no/idporten-oidc-provider/token',
+      jwt: createJwt(jwtOptions)
+    }
+
+    const getDataOptions = {
+      url: 'https://oidc-yt2.difi.eon.no/kontaktinfo-oauth2-server/rest/v1/personer',
+      token: await getToken(tokenOptions),
+      payload: {
+        personidentifikatorer: [ '23079422568' ]
+      }
+    }
+
+    const data = await getData(getDataOptions)
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+})()
